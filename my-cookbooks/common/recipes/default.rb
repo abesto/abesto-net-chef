2cfea0339b6877faa1024bb6c21430dbc6fadff7
Zoltán Nagy <abesto@abesto.net>
2012-07-07 15:35:14 +0200

    Add firewall

--- my-cookbooks/common/recipes/default.rb
+++ my-cookbooks/common/recipes/default.rb
@@ -13,3 +13,19 @@ directory '/opt/my-apps' do
   group 'root'
   action :create
 end
+
+firewall "ufw" do
+  action :enable
+end
+
+firewall_rule "ssh" do
+  port 22
+  protocol :tcp
+  action :allow
+end
+
+firewall_rule "http" do
+  port 80
+  protocol :tcp
+  action :allow
+end
---
+++ my-cookbooks/phpmyadmin/metadata.rb
@@ -0,0 +1,4 @@
+depends 'php'
+depends 'php-fpm'
+depends 'mysql'
+depends 'nginx'
