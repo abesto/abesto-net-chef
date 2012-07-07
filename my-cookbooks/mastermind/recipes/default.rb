1949daae94a2ecc8a131f9f5f88ee765eebfb0a5
Zoltán Nagy <abesto@abesto.net>
2012-07-07 23:37:23 +0200

    Add Mastermind

---
+++ my-cookbooks/mastermind/recipes/default.rb
@@ -0,0 +1,13 @@
+git '/srv/mastermind' do
+  repository 'git://github.com/abesto/mastermind.git'
+  reference 'master'
+  user 'www-data'
+end
+
+template "#{node['nginx']['dir']}/sites-available/mastermind" do
+  source "nginx.erb"
+  owner "root"
+  group "root"
+  mode 0644
+end
+nginx_site 'mastermind'
