dd3efff7cc110fd08cc7a962984532682d2ecb14
Zoltán Nagy <abesto@abesto.net>
2012-07-08 07:26:22 +0200

    Uncomment what needs to be uncommented

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#echo "deb http://apt.opscode.com/ `lsb_release -cs`-0.10 main" | sudo tee /etc/apt/sources.list.d/opscode.list
+echo "deb http://apt.opscode.com/ `lsb_release -cs`-0.10 main" | sudo tee /etc/apt/sources.list.d/opscode.list
 sudo mkdir -p /etc/apt/trusted.gpg.d
 gpg --keyserver keys.gnupg.net --recv-keys 83EF826A
 gpg --export packages@opscode.com | sudo tee /etc/apt/trusted.gpg.d/opscode-keyring.gpg > /dev/null
