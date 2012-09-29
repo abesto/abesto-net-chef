4781ca96387f89da0d7a745e561298fb5bd35d54
Zoltán Nagy <abesto@abesto.net>
2012-09-29 09:25:25 +0200

    Use https://launchpad.net/~chris-lea/+archive/node.js/

--- my-cookbooks/nodejs/recipes/default.rb
+++ my-cookbooks/nodejs/recipes/default.rb
@@ -1,2 +1,12 @@
+apt_repository "nodejs-ppa" do
+  uri "http://ppa.launchpad.net/chris-lea/node.js/ubuntu"
+  distribution node.lsb.codename
+  components ["main"]
+  keyserver "keyserver.ubuntu.com"
+  key "C7917B12"
+  action :add
+  notifies :run, "execute[apt-get update]", :immediately
+end
+
 package 'nodejs' do action :upgrade end
 package 'npm'    do action :upgrade end
