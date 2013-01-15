5b150e911b95a7ff3c04fe05af2d88c33356d676
Zoltan Nagy <abesto@abesto.net>
2013-01-15 20:19:04 +0100

    Really use haproxy; fix: s/template/cookbook_file/

--- my-cookbooks/haproxy/recipes/default.rb
+++ my-cookbooks/haproxy/recipes/default.rb
@@ -1,6 +1,6 @@
 package "haproxy"
 
-template "/etc/haproxy/haproxy.cfg" do
+cookbook_file "/etc/haproxy/haproxy.cfg" do
   source "haproxy.cfg"
   variables(
             :ipaddress => node['ipaddress']
